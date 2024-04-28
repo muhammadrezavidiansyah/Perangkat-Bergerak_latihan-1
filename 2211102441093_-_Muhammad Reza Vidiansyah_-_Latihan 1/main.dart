78c2de3f09b8587cf8b73d57138e1f84a9fab6a2
muhammadrezavidiansyah <145729303+muhammadrezavidiansyah@users.noreply.github.com>
2024-04-28 17:03:44 +0800

    Add files via upload

---
+++ 2211102441093_-_Muhammad Reza Vidiansyah_-_Latihan 1/main.dart
@@ -0,0 +1,25 @@
+import 'package:aplikasipertama/hello_world.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplikasi Flutter Pertama",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Belajar Flutter'),
+        ),
+        body: const Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
